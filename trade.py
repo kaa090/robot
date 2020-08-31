5fe5ffd0989a518986bbef882c376466825595e8
kaa090 <49017533+kaa090@users.noreply.github.com>
2020-08-31 19:55:32 +0300

    Update trade.py

--- trade.py
+++ trade.py
@@ -1,5 +1,5 @@
 class Stock():
-	def __init__(self, price_buy, price_sell, qty, fee_rate = 0.025 / 100):
+	def __init__(self, price_buy, price_sell, qty, fee_rate = 0.05 / 100):
 		self.price_buy = price_buy
 		self.price_sell = price_sell
 		self.qty = qty
@@ -9,11 +9,13 @@ class Stock():
 		fee_sell = self.price_sell * self.qty * self.fee_rate
 		self.fee = fee_buy + fee_sell
 		
-		self.profit = ( self.price_sell - self.price_buy ) * self.qty - self.fee
-		self.prc = ( self.price_sell - self.price_buy ) / self.price_buy * 100
+		self.profit = (self.price_sell - self.price_buy) * self.qty - self.fee
+		self.prc = (self.price_sell - self.price_buy) / self.price_buy * 100
 
-		print("\tprofit = {0}\n\tfee = {1}\n\t% = {2}".format(self.profit, self.fee, self.prc))
+		self.min_price = round(self.price_buy * (1 + self.fee_rate) / (1 - self.fee_rate), 2) + 0.01
 
-# AMD = Stock(48.4, 48.8, 215)
-# TCS = Stock(21.3, 21.35, 100)
-Amazon = Stock(1903.39, 1906, 20)
+		print("\tprofit = {0}\n\tfee = {1}\n\t% = {2}\n\tmin price = {3}"
+				.format(self.profit, self.fee, self.prc, self.min_price))
+
+# myStock = Stock(152.31, 152, 120, 0.05 / 100)
+myStock = Stock(475.5, 315, 10, 0.025 / 100)
